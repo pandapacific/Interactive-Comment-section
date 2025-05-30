f2fdbca617cb9d58a981aebd22211fbb15648f39
Pandapacific <fortuneski88@gmail.com>
2025-05-30 12:14:47 +0100

    commit2

--- script/main.js
+++ script/main.js
@@ -42,9 +42,17 @@ function loadComments(createdClone, x) {
 }
 
 // iterate over each of the comments and render
-data.comments.forEach(x => {
-    // create clone format
+newData.comments.forEach(x => {
+    const storedUserComments = JSON.parse(localStorage.getItem("userComments")) || [];
+
+    storedUserComments.forEach(comment => {
+        loadComments(createClone(comment), comment);
+    });
+
+    // append clone format
     loadComments(createClone(x), x);
+
+
 })
 
 // vote functionality
@@ -52,13 +60,12 @@ const commentContainer = document.getElementById("commentSection"); //locate ove
 let hasVotedUp = false; // check if the user has voted up
 let hasVotedDown = false; // check if the user has voted down
 let hasVoted = false; // check if the user has voted
-// auto scroll-to-bottom functionality
 commentContainer.addEventListener("click", (e) => {
     // locate the buttons
     const plusVote = e.target.closest(".votePlus");
     const minusVote = e.target.closest(".voteMinus");
     let voteCount;
-    
+
     // prevent negative votes
     function resetNegative(voteCount, vote) {
         if (voteCount < 0) {
@@ -76,7 +83,7 @@ commentContainer.addEventListener("click", (e) => {
     }
     // logic
     if (plusVote) {
-        const vote = plusVote.parentElement.querySelector(".voteText"); 
+        const vote = plusVote.parentElement.querySelector(".voteText");
         voteCount = parseInt(vote.textContent); // get the vote count
         if (hasVotedDown) {
             voteCount++;
@@ -92,7 +99,7 @@ commentContainer.addEventListener("click", (e) => {
             checkVoteReset();
         }
     }
-   if (minusVote) {
+    if (minusVote) {
         const vote = minusVote.parentElement.querySelector(".voteText"); //get the parent element of vote button
         voteCount = parseInt(vote.textContent); // get the vote count
         if (hasVotedUp) {
@@ -115,54 +122,63 @@ commentContainer.addEventListener("click", (e) => {
 // send comment functionality
 const mainCommentForm = document.getElementById("commentForm"); //locate main comment form
 const mainSendBtns = mainCommentForm.querySelectorAll(".send"); //locate both send buttons
-//comment logic
+//send comment logic
 mainSendBtns.forEach(btn => {
     btn.addEventListener("click", (e) => {
         e.preventDefault();
         const mainText = mainCommentForm.querySelector(".textBox").value; // locate textarea input
         // comment object
         const userObj = {
-            id : Math.floor((Math.random()) * 10000),
+            id: Math.floor((Math.random()) * 10000),
             content: mainText,
             createdAt: new Date().toISOString(),
             score: 0,
             user: {
-                image: { 
-                png: "./images/avatars/image-juliusomo.png",
-                webp: "./images/avatars/image-juliusomo.webp"
+                image: {
+                    png: "./images/avatars/image-juliusomo.png",
+                    webp: "./images/avatars/image-juliusomo.webp"
                 },
                 username: "juliusomo"
             },
             replies: []
         }
+        // saving to local storage
+        const savedComments = JSON.parse(localStorage.getItem("userComments") || []);
+        savedComments.push(userObj);//add comment
+        localStorage.setItem("userComments", JSON.stringify(savedComments));
+
         // create and append the comment
         loadComments(createClone(userObj), userObj);
         console.log(createClone(userObj));
+
+        // clear textArea
+        mainCommentForm.querySelector(".textBox").value = "";
+
     })
 })
 
 // time calculating function
 function formatTimeAgo(createdAt) {
     const now = new Date();
-    const past =  new Date(createdAt);
-    const seconds = Math.floor((now-past)/1000);
+    const past = new Date(createdAt);
+    const seconds = Math.floor((now - past) / 1000);
     const minutes = Math.floor(seconds / 60);
     const hours = Math.floor(minutes / 60);
     const days = Math.floor(hours / 24);
     const weeks = Math.floor(days / 7);
     const months = Math.floor(weeks / 4)
-    const years = Math.floor(months/12);
+    const years = Math.floor(months / 12);
 
     // logic
-    const timeStamp = 
-    (typeof createdAt === "string" && isNaN(Date.parse(createdAt))) ? createdAt :
-    seconds < 60 ? "Just now" :
-    minutes < 60 ? `${minutes} min${minutes > 1 ? "s" : ""} ago` : 
-    hours < 24 ? `${hours} hour${hours > 1 ? "s" : ""} ago` : 
-    days < 24 ? `${days} day${days > 1 ? "s" : ""} ago` :
-    weeks < 4 ? `${weeks} week${weeks > 1 ? "s" : ""} ago` :
-    months < 12 ? `${months} month${months > 1 ? "s" : ""} ago` :
-    `${years} year${years > 1 ? "s" : ""} ago`;
+    const timeStamp =
+        (typeof createdAt === "string" && isNaN(Date.parse(createdAt))) ? createdAt :
+            seconds < 60 ? "Just now" :
+                minutes < 60 ? `${minutes} min${minutes > 1 ? "s" : ""} ago` :
+                    hours < 24 ? `${hours} hour${hours > 1 ? "s" : ""} ago` :
+                        days < 24 ? `${days} day${days > 1 ? "s" : ""} ago` :
+                            weeks < 4 ? `${weeks} week${weeks > 1 ? "s" : ""} ago` :
+                                months < 12 ? `${months} month${months > 1 ? "s" : ""} ago` :
+                                    `${years} year${years > 1 ? "s" : ""} ago`;
     return timeStamp
 }
 // time updating function
@@ -171,7 +187,6 @@ function updateTimestamps() {
     timeElements.forEach(el => {
         const isoTime = el.dataset.time;
         const newText = formatTimeAgo(isoTime);
-        // console.log(newText);
         el.innerHTML = newText;
     });
 }
@@ -179,38 +194,4 @@ function updateTimestamps() {
 // update every 30 seconds
 setInterval(updateTimestamps, 30000);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// send comments functionality
-// const commentForm = document.querySelectorAll(".commentForm"); //locate all available document forms
-// // console.log(commentForm);
-
-// commentForm.forEach(form => {
-//     // define the screen test function
-    
-//     screenTest();
-//     window.addEventListener("resize", screenTest())
-// })
-// console.log(text);
-// send.for
-// function sendComment() {
-//     const user = {
-//         name: "juliusomo",
-//         profileImage: "./images/avatars/image-juliusomo.png",
-//         createdAt: "",
-//         // text = document.getElementsByClassName("textBox").value,
-
-//     };
-// }
-// send.addEventListener("click", sendComment());
+// local storage
